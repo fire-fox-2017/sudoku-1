f0c47454d94562b92d2b2c9c577839074133ba11
fearzen3 <ivanhabi2@gmail.com>
2017-03-31 17:23:31 +0700

    selse

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,131 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+    this.string=board_string.split("");
+    this.boardUrut=[];
+    this.zona=[];
+    this.digit=['1','2','3','4','5','6','7','8','9',];
+  }
 
-  solve() {}
+
+
+  solve() {
+
+    function hapusKosong(arr){
+
+      let index;
+      for(let i=0;i<arr.length;i++){
+        index = arr.indexOf('0');
+        if (index > -1) {
+        arr.splice(index, 1);
+        }
+      }
+      return arr;
+    }
+    for(let i = 0;i <=(9*9)-(9-1) ;i=i+9 ){
+      this.boardUrut.push(this.string.slice(i,(i+9)));
+    }
+
+
+    function isiKotak(x,y,arr,temp){
+      let baris=x;
+      let kolom=y;
+      let bool=false;
+      for(let i=x;i<=(baris+2);i++){
+        for(let j=y;j<=(kolom+2);j++){
+          if((arr[i][j]!=='0')){
+
+            temp[temp.indexOf(arr[i][j])]='0';
+          }
+        }
+      }
+      temp =hapusKosong(temp);
+      console.log(temp);
+
+      for(let i=x;i<=(baris+2);i++){
+        for(let j=y;j<=(kolom+2);j++){
+
+          if(arr[i][j]==='0'){
+
+            //replace dengan temp
+            for(let k=0;k<temp.length;k++){
+
+              if(checkBaris(temp[k],i,arr)&&checkKolom(temp[k],j,arr)){
+                arr[i][j]=temp[k];
+                bool=true;
+                break;
+              }
+            }
+            if(bool===false){
+              arr[i][j]=String(Math.floor((Math.random() * 9) + 1));
+            }else{
+              bool=false;
+
+            }
+
+
+
+
+          }
+        }
+      }
+
+      return arr;
+    }
+
+    function checkBaris(n,x,arr){
+      let bool=false;
+      for(let i=x;i<=x;i++){
+        for(let j=0;j<=8;j++){
+          if(arr[i][j]===n){
+            return false
+          }
+        }
+      }
+      return true
+    }
+
+    function checkKolom(n,y,arr){
+      for(let i=0;i<=8;i++){
+        for(let j=y;j<=y;j++){
+            if(arr[i][j]===n){
+              return false
+            }
+        }
+      }
+      return true
+    }
+
+
+    this.boardUrut = isiKotak(0,0,this.boardUrut,this.digit);
+    this.digit=['1','2','3','4','5','6','7','8','9',]
+    this.boardUrut = isiKotak(0,3,this.boardUrut,this.digit);
+    this.digit=['1','2','3','4','5','6','7','8','9',]
+    this.boardUrut = isiKotak(0,6,this.boardUrut,this.digit);
+    this.digit=['1','2','3','4','5','6','7','8','9',]
+    this.boardUrut = isiKotak(3,0,this.boardUrut,this.digit);
+    this.digit=['1','2','3','4','5','6','7','8','9',]
+    this.boardUrut = isiKotak(3,3,this.boardUrut,this.digit);
+    this.digit=['1','2','3','4','5','6','7','8','9',]
+    this.boardUrut = isiKotak(3,6,this.boardUrut,this.digit);
+    this.digit=['1','2','3','4','5','6','7','8','9',]
+    this.boardUrut = isiKotak(6,0,this.boardUrut,this.digit);
+    this.digit=['1','2','3','4','5','6','7','8','9',]
+    this.boardUrut = isiKotak(6,3,this.boardUrut,this.digit);
+    this.digit=['1','2','3','4','5','6','7','8','9',]
+    this.boardUrut = isiKotak(6,6,this.boardUrut,this.digit);
+
+  }
 
   // Returns a string representing the current state of the board
-  board() {}
+  board() {
+
+
+    return this.boardUrut;
+
+
+  }
 }
 
 // The file has newlines at the end of each line,
