1459722d1b911c6fdef66528073b8991e5cfc36b
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-03-31 12:39:04 +0800

    print board done

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,44 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+    this._board_string = board_string;
+    this._board = [];
+    this.createBoard();
 
-  solve() {}
+  }
+
+  createBoard() {
+    let temp = this._board_string.split('');
+    for (let i = 0 ; i < temp.length ; i++) {
+      this._board.push(temp[i]);
+    }
+
+  }
+  solve() {
+    console.log("solve!");
+  }
 
   // Returns a string representing the current state of the board
-  board() {}
+  board() {
+    let str = "";
+    for (let i = 0 ; i < 9 ; i++) {
+      if ( i % 3 === 0)
+        str += "---------------------\n";
+      for (let j = 0 ; j < 9 ; j++) {
+        if (j % 3 === 0 && j != 0)
+          str += "| ";
+        str += this._board[i+j] + " ";
+      }
+      str += "\n";
+    }
+    str += "---------------------\n";
+
+    // console.log("\n");
+    // console.log(str);
+    // console.log("\n");
+    return str;
+  }
 }
 
 // The file has newlines at the end of each line,
@@ -16,9 +48,11 @@ var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
   .toString()
   .split("\n")[0]
 
-var game = new Sudoku(board_string)
+console.log(board_string);
+
+var game = new Sudoku(board_string);
 
 // Remember: this will just fill out what it can and not "guess"
-game.solve()
+game.solve();
 
-console.log(game.board())
+console.log(game.board());
